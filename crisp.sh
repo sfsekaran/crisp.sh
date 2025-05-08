587b3611f1ede4541f7d9c419d2fd2616eb31cbd
Sathya Sekaran <sfsekaran@gmail.com>
2025-05-07 18:32:06 -0700

    update for latest softwares

--- crisp.sh
+++ crisp.sh
@@ -1,17 +1,19 @@
 # installs only the basics
 
-# install homebrew first
-# TODO: check if exists before installing
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install visual-studio-code
-brew install autojump git-flow tig
-
 # install oh_my_zsh
 # TODO: check if exists before installing
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install homebrew
+# TODO: check if exists before installing
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /Users/sathya/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/sathya/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install --cask google-chrome iterm2 visual-studio-code
+brew install autojump git-flow tig
 
 # setup git aliases
 git config --global alias.co checkout &&
